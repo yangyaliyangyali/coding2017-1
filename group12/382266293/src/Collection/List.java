93e0b1d28bfafa21032887825e93eff6d9d18c3a
OnlyLYJ <382266293@qq.com>
2017-02-20 07:01:54 +0800

    ArrayList added

---
+++ group12/382266293/src/Collection/List.java
@@ -0,0 +1,25 @@
+package Collection;
+
+public interface List<E> {
+	
+	public void add(E e);
+	
+	public void add(int index, E e);
+	
+	public E remove(int index);
+	
+	public int size();
+	
+	public E get(int index);
+	
+	public boolean isEmpty();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
