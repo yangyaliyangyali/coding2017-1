5b12f30be2d0c91ddc1dea2f463561cf059d6dcf
guozihang <15047271937@163.com>
2017-02-22 09:29:39 +0800

    delete .meta

--- group21/648084221/CainaTest/src/cc/gzhlaker/blog/MainTest.java
+++ group21/648084221/CainaTest/src/cc/gzhlaker/blog/MainTest.java
@@ -4,10 +4,18 @@ public class MainTest {
 	
 	public static void main(String[] args)
 	{
-		
 		String a = "Hello World";
 		System.out.println(a);
 		
 	}
 
 }
+class MyData{
+	
+	int number;
+	
+	public MyData(){
+		number = 0;
+	}
+}
+
